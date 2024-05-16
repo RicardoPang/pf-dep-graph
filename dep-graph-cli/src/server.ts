5ac12d4084a8ca34ab1b99327041e6e3cfbc5cc5
RicardoPang <ricardo.pangj@gmail.com>
2024-05-16 16:36:43 +0800

    fix: 已知bug修复

--- dep-graph-cli/src/server.ts
+++ dep-graph-cli/src/server.ts
@@ -32,7 +32,6 @@ export const startServer = async (
   json: string,
   isParseLockFile: boolean
 ) => {
-  debugger
   // 解析锁文件(isParseLockFile 为 true)
   if (isParseLockFile) {
     // 定义不同锁文件类型对应的处理类
@@ -126,7 +125,6 @@ const startApiServer = async (
       let graph: IGraphProps[] = []
       let nodeArray: INodeArrayProps[] = []
 
-      debugger
       // 根据isParseLockFile参数判断是否解析锁文件
       if (isParseLockFile) {
         graph = lockData.graph
@@ -196,9 +194,9 @@ const startApiServer = async (
   })
 
   // TODO: 打开浏览器(测试关闭)
-  // await open(`http://localhost:${CLIENT_PORT}/`, {
-  //   app: { name: 'google chrome', arguments: ['--disable-extensions'] }
-  // })
+  await open(`http://localhost:${CLIENT_PORT}/`, {
+    app: { name: 'google chrome', arguments: ['--disable-extensions'] }
+  })
 }
 
 // 生成依赖关系图并保存到文件
